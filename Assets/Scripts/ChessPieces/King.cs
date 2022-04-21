dbbf6f104d226f71bc41060fed172ee6b1d6d6a0
yasser74220 <yasserahlawy970@gmail.com>
2022-04-21 23:08:03 +0200

    3D Unity

---
+++ Assets/Scripts/ChessPieces/King.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class King : ChessPiece
+{
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> r = new List<Vector2Int>();
+        if (currentX + 1 < tileCountX) // right
+        {
+            if (board[currentX + 1, currentY] == null)
+            {
+                r.Add(new Vector2Int(currentX + 1, currentY));
+            }
+            else if (board[currentX + 1, currentY].team != team)
+            {
+                r.Add(new Vector2Int(currentX + 1, currentY));
+            }
+
+
+            // top right
+            if (currentY + 1 < tileCountY)
+            {
+                if (board[currentX + 1, currentY + 1] == null)
+                {
+                    r.Add(new Vector2Int(currentX + 1, currentY + 1));
+                }
+                else if (board[currentX + 1, currentY + 1].team != team)
+                {
+                    r.Add(new Vector2Int(currentX + 1, currentY + 1));
+                }
+            }
+            // bottom right
+            if (currentY - 1 >= 0)
+            {
+                if (board[currentX + 1, currentY - 1] == null)
+                {
+                    r.Add(new Vector2Int(currentX + 1, currentY - 1));
+                }
+                else if (board[currentX + 1, currentY - 1].team != team)
+                {
+                    r.Add(new Vector2Int(currentX + 1, currentY - 1));
+                }
+            }
+        }
+        //left
+        if (currentX - 1 >= 0)
+        {
+            if (board[currentX - 1, currentY] == null)
+            {
+                r.Add(new Vector2Int(currentX - 1, currentY));
+            }
+            else if (board[currentX - 1, currentY].team != team)
+            {
+                r.Add(new Vector2Int(currentX - 1, currentY));
+            }
+
+
+            // top left
+            if (currentY + 1 < tileCountY)
+            {
+                if (board[currentX - 1, currentY + 1] == null)
+                {
+                    r.Add(new Vector2Int(currentX - 1, currentY + 1));
+                }
+                else if (board[currentX - 1, currentY + 1].team != team)
+                {
+                    r.Add(new Vector2Int(currentX - 1, currentY + 1));
+                }
+            }
+            // bottom left
+            if (currentY - 1 >= 0)
+            {
+                if (board[currentX - 1, currentY - 1] == null)
+                {
+                    r.Add(new Vector2Int(currentX - 1, currentY - 1));
+                }
+                else if (board[currentX - 1, currentY - 1].team != team)
+                {
+                    r.Add(new Vector2Int(currentX - 1, currentY - 1));
+                }
+            }
+        }
+        //up
+        if(currentX+1 <tileCountY)
+            if(board[currentX,currentY+1]==null || board[currentX,currentY+1].team!= team)
+            {
+                r.Add(new Vector2Int(currentX, currentY + 1));
+            }
+        //down
+        if (currentY -1 >= 0)
+            if (board[currentX, currentY - 1] == null || board[currentX, currentY - 1].team != team)
+            {
+                r.Add(new Vector2Int(currentX, currentY - 1));
+            }
+        return r;
+    }
+}
